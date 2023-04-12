9d64e440610e31fa5ec542835c8359a876fc7a92
unknown <satya-1298>
2023-04-12 20:31:40 +0530

    Call method

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,7 +7,25 @@ namespace AddressBook
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            AddressBookDictonary dict = new AddressBookDictonary();
+            bool flag = true;
+            while (flag)
+            {
+                Console.Write("1.Add Contact\n2.Display\n3.Exit\nEnter your option: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        dict.AddAddress();
+                        break;
+                    case 2:
+                        dict.Display();
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
